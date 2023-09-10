95f1609586f91b1cd9067edd97b6b9fb66a7a20a
priyanshikotian0902 <priyanshi.kotian@mitaoe.ac.in>
2023-09-10 11:06:18 +0530

    new

---
+++ hello4.java
@@ -0,0 +1,9 @@
+public class hello4
+{
+    public static void main(String arg[])
+    {
+       // System.out.println("helooooo...");
+        System.out.println("priyasnhi...");
+         System.out.println("kotian...");
+    }
+}
